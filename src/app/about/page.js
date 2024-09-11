dcb9a08db652baf4b67f49b5778767e9a4569ac8
Megan Palmquist <megpalmq@iu.edu>
2024-09-11 11:03:14 -0400

    update gloabal css to ass apdding to all sections across app
    Added section to about page

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -12,15 +12,17 @@ export default function About(){
           <li><a href="#">Contact</a></li>
         </ul>
       </nav>
+      <section>
         <h2>Meg Stone</h2>
         <h3>Intersts</h3>
         <ul>
             <li>coding</li>
             <li>gym</li>
-            <li>video games</li>
+            <li>fnaf</li>
             <li>fashion</li>
             <li>Pad See Ew </li>
         </ul>
+        </section>
         </main>
     );
 }
